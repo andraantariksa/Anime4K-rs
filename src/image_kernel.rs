a82c5c32db45450e88152002d5a997305513d9b3
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2019-10-26 23:21:41 +0700

    Added computed luminance of an image

--- src/image_kernel.rs
+++ src/image_kernel.rs
@@ -1,4 +1,6 @@
-pub fn clamp(val: i32, min: i32, max: i32) -> i32 {
+use std::cmp::PartialOrd;
+
+pub fn clamp<T:PartialOrd>(val: T, min: T, max: T) -> T {
     if val < min {
         min
     } else if val > max {
@@ -8,20 +10,47 @@ pub fn clamp(val: i32, min: i32, max: i32) -> i32 {
     }
 }
 
+pub fn extract_pixel_rgba(pixel: &image::Rgba<u8>) -> (u8, u8, u8, u8) {
+    (pixel.0[0], pixel.0[1], pixel.0[2], pixel.0[3])
+}
+
+// https://stackoverflow.com/a/596241/3894179
+pub fn get_brightness(r: u8, g: u8, b: u8) -> u8 {
+    ((r as u16 + r as u16 + g as u16 + g as u16 + g as u16 + b as u16) / 6) as u8
+}
+
 pub struct ImageKernel {
-    pub image: image::DynamicImage,
+    pub image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
     pub run_type: i32,
     pub unblur_strength: u8,
     pub refine_strength: u8,
+    pub color_data: Vec<u32>,
 }
 
 impl ImageKernel {
-    fn from_image(image: image::DynamicImage) -> ImageKernel {
+    pub fn from_image(image: image::DynamicImage) -> ImageKernel {
         ImageKernel {
-            image,
+            image: image.to_rgba(),
             run_type: 0,
             unblur_strength: 0,
             refine_strength: 0,
+            color_data: Vec::new(),
+        }
+    }
+
+    pub fn compute_luminance(&mut self) {
+        for y in 0..self.image.height() {
+            for x in 0..self.image.width() {
+                let mut pixel = self.image.get_pixel_mut(x, y);
+                let (r, g, b, _) = extract_pixel_rgba(&pixel);
+                let brightness = get_brightness(r, g, b);
+                let luminance_value = clamp(brightness, 0, 0xFF);
+
+                pixel.0[0] = r;
+                pixel.0[1] = r;
+                pixel.0[2] = r;
+                pixel.0[3] = luminance_value;
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate image;
 
-use std::{env, process, fs};
-// use image::ImageBuffer;
+use std::{env, process};
 
 mod image_kernel;
 
@@ -11,13 +10,14 @@ fn main() {
         process::exit(1);
     }
     let args: Vec<String> = env::args().collect::<Vec<String>>();
-    let input_filename = &args[1]; // Debug
+
+    let input_filename = &args[1];
     let output_filename = &args[2];
 
-    // let input_file_content = fs::read_to_string(input_filename).expect("Can't read the file");
-
-    // let image_input_buffer = ImageBuffer::from_raw(100, 100, );
-
     let image = image::open(&input_filename).expect("Can't open image");
+
+    let mut kernel_instance = image_kernel::ImageKernel::from_image(image);
+    kernel_instance.compute_luminance();
+    kernel_instance.image.save(output_filename).expect("Can't save image");
 
 }
