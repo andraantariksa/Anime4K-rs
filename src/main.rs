cd65c17bce6b4fe1713e8e1474bf3dff0b54e2e8
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2019-10-27 16:46:00 +0700

    Using clap crates as an interface

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,79 @@
+extern crate clap;
 extern crate image;
+extern crate raster;
 
-use std::{env, process};
+use clap::{App, Arg};
 
 mod image_kernel;
 
 fn main() {
-    if env::args().len() < 2 {
-        println!("Error: Please specify input and output png files.");
-        process::exit(1);
-    }
-    let args: Vec<String> = env::args().collect::<Vec<String>>();
+    let matches = App::new("Anime4K-rs")
+        .version("0.1")
+        .author("Andra Antariksa <andra.antariksa@gmail.com>")
+        .about("A High-Quality Real Time Upscaler for Anime Video")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Sets the output file")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("scale")
+                .short("s")
+                .long("scale")
+                .default_value("2")
+                .help("Sets the input file to use"),
+        )
+        .arg(
+            Arg::with_name("iteration")
+                .short("i")
+                .long("iteration")
+                .default_value("2")
+                .help("Sets how many the iteration to do"),
+        )
+        .arg(
+            Arg::with_name("push-color-strength")
+                .long("pcs")
+                .default_value("0")
+                .help("Sets the push color strength"),
+        )
+        .arg(
+            Arg::with_name("push-gradient-strength")
+                .long("pgs")
+                .default_value("1")
+                .help("Sets push gradient strength"),
+        )
+        .get_matches();
 
-    let input_filename = &args[1];
-    let output_filename = &args[2];
+    let input_filename = matches
+        .value_of("INPUT")
+        .expect("Error: Please specify input and output png files.");
+    let output_filename = matches
+        .value_of("OUTPUT")
+        .expect("Error: Please specify input and output png files.");
+    let scale = matches.value_of("scale").unwrap().parse::<f64>().expect("Error on parsing scale to f64");
+    let iteration = matches.value_of("iteration").unwrap().parse::<u8>().expect("Error on parsing iteration to u8");
+    let push_color_strength = matches.value_of("push-color-strength").unwrap().parse::<f64>().expect("Error on parsing push-color-strength to f64");
+    let push_gradient_strength = matches.value_of("push-gradient-strength").unwrap().parse::<f64>().expect("Error on parsing push-gradient-strength to f64");
 
-    let image = image::open(&input_filename).expect("Can't open image");
+    let image = image::open(&input_filename).expect("Can't open image.");
 
     let mut kernel_instance = image_kernel::ImageKernel::from_image(image);
-    kernel_instance.compute_luminance();
-    kernel_instance.image.save(output_filename).expect("Can't save image");
-
+    kernel_instance.scale(
+        kernel_instance.width() * scale as u32,
+        kernel_instance.height() * scale as u32,
+    );
+    for _ in 0..iteration {
+        kernel_instance.compute_luminance();
+        kernel_instance.push_color(image_kernel::clamp((push_color_strength * 255.0) as u16, 0, 0xFFFF));
+        kernel_instance.compute_gradient();
+        kernel_instance.push_gradient(image_kernel::clamp((push_gradient_strength * 255.0) as u16, 0, 0xFFFF));
+    }
+    kernel_instance
+        .save(output_filename)
+        .expect("Can't save image.");
 }
